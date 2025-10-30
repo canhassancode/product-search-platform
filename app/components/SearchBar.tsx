421ee0fdca9866412fd15b9b7b7596d1b23bc440
hassan <hassan@oneforge.io>
2025-10-30 15:09:46 +0000

    search icon used on search bar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
+import LordIcon from "@/components/LordIcon";
 
 export default function SearchBar({ query, setQuery }: { query: string; setQuery: (query: string) => void }) {
   return (
-    <div className="flex items-center gap-2">
-      <Input type="text" placeholder="Search products" className="bg-white" value={query} onChange={(e) => setQuery(e.target.value)} />
+    <div className="flex items-center gap-2 relative">
+      <Input
+        type="text"
+        placeholder="Search products..."
+        className="bg-white border border-gray-400 absolute left-0"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <div className="absolute right-0 flex items-center justify-center pr-2">
+        <LordIcon iconName="search" size={20} />
+      </div>
     </div>
   );
 }
